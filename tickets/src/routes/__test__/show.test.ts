2cd14cc880e40e7ca5cdf537d56333b4d3d79d6f
abhayg30 <abhaygupta.3003@gmail.com>
2024-12-17 14:04:10 +1100

    Tests for getting tickets using TDD

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,28 @@
 import request from 'supertest';
 import { app } from '../../app';
+import exp from 'constants';
 
 it('returns 404 if ticket not found', async () =>{
     await request(app)
         .get('/api/tickets/abcder')
         .send()
         .expect(404)
+})
+
+it('returns the ticket if the ticket is found', async()=>{
+    const title = 'ticket123'
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: title,
+            price: 20
+        })
+        .expect(201)
+    const ticketResponseData = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200)
+    expect(ticketResponseData.body.title).toEqual(title)
+    expect(ticketResponseData.body.price).toEqual(20)
 })
