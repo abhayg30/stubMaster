85623e83d136c0fe207823f6dd63a91fbe517d4e
abhayg30 <abhaygupta.3003@gmail.com>
2024-12-31 10:41:18 +1100

    client updated

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,41 @@
-import axios from "axios";
-import BuildClient from "../api/build-client";
+import Link from 'next/link';
 
-//we could use any hook inside a component not inside initialProps hence we are using axios
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            View
+          </Link>
+        </td>
+      </tr>
+    );
+  });
 
-const LandingPage = ({ currentUser }) => {
-    return currentUser ? <h1>You are singed in</h1> : <h1>You are NOT signed in</h1>
-}
-//server makes request
-LandingPage.getInitialProps = async (context) => {
-  const client = BuildClient(context)
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
-}
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
+};
+
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
+};
 
 export default LandingPage;
-
-
